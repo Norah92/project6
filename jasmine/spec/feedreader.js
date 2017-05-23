3db71a7aa19d9f3bbe01d58191a1aed4695bcd62
Norah92 <nora_altwaim@yahoo.com>
2017-05-23 12:02:06 +0300

    Update feedreader.js

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -12,7 +12,6 @@
        * a related set of tests. This suite is all about the RSS
        * feeds definitions, the allFeeds variable in our application.
        */
-	
     	describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -20,20 +19,15 @@
          * the rest of this project. What happens when you change
          * allFeeds in app.js to be an empty array and refresh the
          * page?
-         */
-		
-		 
+         */	 
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
-         */
-		 
+         */	 
         it('has a URL defined', function() {
 	for (var i = 0; i<allFeeds.length; i++) {
 	   expect(allFeeds[i].url).toBeDefined();
@@ -44,8 +38,7 @@
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
-         */
-		 
+         */	 
 	it('has a name defined', function() {
 	for (var i = 0; i<allFeeds.length; i++) {
 	   expect(allFeeds[i].name).toBeDefined();
@@ -57,7 +50,6 @@
     });
     /* TODO: Write a new test suite named "The menu" */
 	describe('The menu', function() {
-		
        /* TODO: Write a test that ensures the menu element is
         * hidden by default. You'll have to analyze the HTML and
         * the CSS to determine how we're performing the
@@ -66,12 +58,11 @@
 	it('is hidden by default', function() {
 	expect($('body').hasClass('menu-hidden')).toBe(true);
 	     });
-	 /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-		
+	/* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
 	it(' menu changes visibility when clicked', function() {
 	$('.menu-icon-link').trigger('click');
 	xpect($('body').hasClass('menu-hidden')).toBe(false); 
@@ -82,10 +73,10 @@
 	expect($('body').hasClass('menu-hidden')).toBe(true); 
 		});
 	});
-    /* TODO: Write a new test suite named "Initial Entries" */
+        /* TODO: Write a new test suite named "Initial Entries" */
 	
 	describe('Initial Entries', function() {
-		/* TODO: Write a test that ensures when the loadFeed
+	/* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -95,14 +86,12 @@
 	loadFeed(0, function() {
 	done();
 			});			
-		}); 
-			
+		}); 		
 	it('there is at least a single .entry element within the .feed container', function() {
 	expect($('.feed .entry').length).not.toBe(0);
 		});
 	});
         /* TODO: Write a new test suite named "New Feed Selection" */
-	
 	describe('New Feed Selection', function() {
 	/* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
@@ -125,7 +114,5 @@
 			});
 			
 		});
-	});
-
-        
+	});       
 }());
